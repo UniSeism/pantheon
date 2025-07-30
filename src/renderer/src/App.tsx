761f4ca3f47bfaf2c9ea0b66cb622fd205e489ab
CRooi <contact@crooi.io>
2025-07-30 19:50:18 +0800

    init: init repository

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@medusajs/ui'
+
+function App() {
+    return <>
+        <Button>Hello World</Button>
+        <div className='bg-red-500'>1</div>
+    </>
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/renderer/index.html",
+    "./src/renderer/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@medusajs/ui/dist/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  presets: [require("@medusajs/ui-preset")]
+}
+
